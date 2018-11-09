34e7bcfcf26982545ed9e0cef4f7a77a638c1263
fatmamoalla <37354761+fatmamoalla@users.noreply.github.com>
2018-11-10 00:41:56 +0100

    Update Code Algos ML.py

--- Code Algos ML.py
+++ Code Algos ML.py
@@ -4,7 +4,8 @@ import numpy as np
 import math
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.ensemble import GradientBoostingClassifier
 
 #Loading data:
 
@@ -30,3 +31,19 @@ WIP...
 
 
 #RandomForest
+
+
+
+
+
+#Boosting
+learning_rates = [0.05, 0.1, 0.25, 0.5, 0.75, 1]
+param_grid={'learning_rate':learning_rates}
+gb = GradientBoostingClassifier(n_estimators=20, max_features=2, max_depth = 2, random_state = 0)
+best_gb=GridSearchCV(estimator, param_grid, cv=20)
+best_gb.fit(X_train, y_train)
+print("Learning rate: ", best_gb.best_params_)
+print("Accuracy score (training): {0:.4f}".format(best_gb.score(X_train, y_train)))
+print("Accuracy score (testing): {0:.4f}".format(best_gb.score(X_test, y_test)))
+
+
