7626a6740438485e04c4474e48b1e8ac20e5a286
Palmyre <palmyra.wuertz@polytechnique.edu>
2018-11-10 00:23:31 +0100

    Add description of function in script

--- matrix.py
+++ matrix.py
@@ -8,6 +8,12 @@ folder = '/home/palmyre/Documents/Huawei Data Challenge/sub_training_set/'
 #     return sum( buf.count(b'\n') for buf in bufgen )
 
 def process(i_beg = 1, i_end = 6):
+    """
+    Create a file `matrix.txt` with number of processes and number of nodes (lines).
+
+    i_beg : index of first sample
+    i_end : index+1 of last sample
+    """
     matrix = []
     for i in range (i_beg, i_end):
         index = str(i).zfill(6)
@@ -15,8 +21,6 @@ def process(i_beg = 1, i_end = 6):
             processes = set()
             for line in f:
                 child = re.search(u'(?<=-> )\w+\.exe', line)
-                if child.group(0) not in processes:
-                    print(child.group(0))
                 processes.add(child.group(0))
             nb_processes = len(processes)
         with open(folder + 'training_{}_behavior_sequence.txt'.format(index), 'r') as f:
