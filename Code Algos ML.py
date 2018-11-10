8b2315dde328c6a1aa0b4f96e4cfdaf02efb8b2c
fatmamoalla <37354761+fatmamoalla@users.noreply.github.com>
2018-11-10 05:34:40 +0100

    Update Code Algos ML.py

--- Code Algos ML.py
+++ Code Algos ML.py
@@ -6,7 +6,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.metrics import roc_curve
-from sklearn.metrics: import auc
+from sklearn.metrics import auc
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
