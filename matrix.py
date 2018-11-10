c836925b707b0e907557443451a543c63546a169
Palmyre <palmyra.wuertz@polytechnique.edu>
2018-11-10 01:04:52 +0100

    Hard-coded path and bounds

--- matrix.py
+++ matrix.py
@@ -2,28 +2,24 @@
 import re
 import os
 
-folder = str(os.path.dirname(os.path.realpath(__file__))) + '/training_dataset/'
-
 # def count_lines(f):
 #     bufgen = takewhile(lambda x: x, (f.raw.read(1024*1024) for _ in repeat(None)))
 #     return sum( buf.count(b'\n') for buf in bufgen )
 
-def process(i_beg = 1, i_end = 200000):
+def process():
     """
     Create a file `matrix.txt` with number of processes and number of nodes (lines).
-    Files have to be in a folder named 'training_dataset'.
-
-    i_beg : index of first sample
-    i_end : index+1 of last sample
     """
+    folder = '/home/cloud/hackathon/training_dataset/'
     matrix = []
-    for i in range (i_beg, i_end):
+    for i in range (1, 20000):
         index = str(i).zfill(6)
         with open(folder + 'training_{}_process_generation.txt'.format(index), 'r') as f:
             processes = set()
             for line in f:
                 child = re.search(u'(?<=-> )\w+\.exe', line)
-                processes.add(child.group(0))
+                if  child:
+                    processes.add(child.group(0))
             nb_processes = len(processes)
         with open(folder + 'training_{}_behavior_sequence.txt'.format(index), 'r') as f:
             nb_lines =  sum(1 for line in f)
