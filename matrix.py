3cc732cbaf0ad8734b76a476cf4db394bb756b41
Palmyre <palmyra.wuertz@polytechnique.edu>
2018-11-10 02:14:03 +0100

    Return np array, do not write file anymore

--- matrix.py
+++ matrix.py
@@ -1,30 +1,38 @@
 # from itertools import (takewhile,repeat)
-import re
+import numpy as np
 import os
+import re
+
 
 # def count_lines(f):
-#     bufgen = takewhile(lambda x: x, (f.raw.read(1024*1024) for _ in repeat(None)))
+#     """
+#     Fast line counter.
+#     f has to be opened as a binary ('rb').
+#     """
+#     bufgen = takewhile(lambda x: x, (f.read(1024*1024) for _ in repeat(None)))
 #     return sum( buf.count(b'\n') for buf in bufgen )
 
 def process():
     """
-    Create a file `matrix.txt` with number of processes and number of nodes (lines).
+    Return a NumPy array with as many lines as samples,
+    and two columns:
+    - couples (parent, child)
+    - lines of behavior_sequence files.
     """
-    folder = '/home/cloud/hackathon/training_dataset/'
+    folder = '/home/cloud/hackathon/'
+    training_dir = folder + 'training_dataset/'
+    matrix_dir = folder + 'palmyre/'
     matrix = []
+
     for i in range (1, 20000):
         index = str(i).zfill(6)
-        with open(folder + 'training_{}_process_generation.txt'.format(index), 'r') as f:
-            processes = set()
+        with open(training_dir + 'training_{}_process_generation.txt'.format(index), 'r') as f:
+            processes = []
             for line in f:
-                spawn = re.search(u'(\w+).exe -> (\w+)\.exe', line)
+                spawn = re.search(u'(.*) -> (.*)', line)
                 if  spawn:
-                    processes.add((spawn.group(1), spawn.group(2)))
-        with open(folder + 'training_{}_behavior_sequence.txt'.format(index), 'r') as f:
+                    processes.append((spawn.group(1), spawn.group(2)))
+        with open(training_dir + 'training_{}_behavior_sequence.txt'.format(index), 'r') as f:
             lines = [line for line in f]
         matrix.append([processes, lines])
-    with open(folder + 'matrix.txt', 'w') as m:
-        m.write(str(matrix))
-
-if __name__ == '__main__':
-    process()
+    return np.array(matrix)
