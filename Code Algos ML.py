387ade3308509b0494bdf1c50344fae2f39bf506
antoinedelplace <34864698+antoinedelplace@users.noreply.github.com>
2018-11-10 07:50:13 +0100

    Update Code Algos ML.py

--- Code Algos ML.py
+++ Code Algos ML.py
@@ -1,152 +1,254 @@
-###I- Importing useful libraries
-
+#! python3.6
+# -*- coding: cp1252 -*-
 import numpy as np
-import math
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from pylab import savefig
 import pandas as pd
-from sklearn.model_selection import train_test_split #GridSearchCV
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.metrics import roc_curve
-from sklearn.metrics import auc
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
-import shutil
+import re
+from sklearn import linear_model, decomposition, metrics, model_selection, preprocessing
+import seaborn as sns
+from numpy import linalg
+import pprint
+import time
+
+import warnings
+warnings.filterwarnings("ignore")
+
+start_time = time.time()
+print("----------------------------------------Import matrix--------------------------------------------------------------")
+#matrix_test = np.array([[[('stub.exe_9f01000', 'sample.exe_1356d000')], ['sample.exe_1356d000,1fa617,api_1487\n', 'sample.exe_1356d000,1fa626,api_0496\n', 'sample.exe_1356d000,1fa62f,api_0338\n']], [[('stub.exe_9f01000', 'sample.exe_1356d000')], ['sample.exe_1356d000,1fa617,api_1487\n', 'sample.exe_1356d000,1fa626,api_0496\n', 'sample.exe_1356d000,1fa62f,api_0338\n']], [[('stub.exe_1319c000', 'sample.exe_11ad0000'), ('stub.exe_1319c000', 'sample.exe_11ad0000')], ['sample.exe_1356d000,1fa617,api_1487\n', 'sample.exe_1356d000,1fa626,api_0496\n', 'sample.exe_1356d000,1fa62f,api_0338\n']]])
+matrix_test_total = np.load("../palmyre/matrix.raw")
+matrix_test = matrix_test_total[0:1000, :]
+N = len(matrix_test)
+
+end_time = time.time()
+print("----------------------------------------Done in "+str(end_time-start_time)+" s.------------------------------------------------------")
+start_time = time.time()
+
+start_time = time.time()
+print("----------------------------------------Import label--------------------------------------------------------------")
+with open('../true_labels_training.txt') as f:
+    lines = f.readlines()
+labels=np.array([int(lines[0][i]) for i in range(0,len(lines[0]))])
+
+pprint.pprint(labels)
+
+end_time = time.time()
+print("----------------------------------------Done in "+str(end_time-start_time)+" s.------------------------------------------------------")
+start_time = time.time()
+
+def clean(line):
+    for i in range(0, len(line[1])):
+        string = re.sub("\n", "", line[1][i])
+        line[1][i] = np.array(string.split(","))
+        
+print("----------------------------------------Cleaning--------------------------------------------------------------")
+np.apply_along_axis(lambda line: clean(line), 1, matrix_test)
+
+end_time = time.time()
+print("----------------------------------------Done in "+str(end_time-start_time)+" s.------------------------------------------------------")
+start_time = time.time()
+
+p = 13 #Nb de features
+X = []
+
+print("----------------------------------------Feature Engineering------------------------------------------------------")
+count_line_tab = []
+
+def count(line):
+    unique, counts = np.unique(line, return_counts=True)
+    return dict(zip(unique, counts))
+
+#Count
+np.apply_along_axis(lambda line: count_line_tab.append(count(line[1])), 1, matrix_test)
+
+npcount_line_tab = np.array(count_line_tab)
 
 
-###II- Loading data:
-X=np.load("antoine/main.py") #1000 raws
-y= np.load("true_labels_training.txt")
-X_train, X_test, y_train, y_test= train_test_split(X,y,test_size=0.2)
-#X_val=np.load(
+def count_max_same_api_call(dict_line):
+    maxi = 0
+    for key in dict_line:
+        if str(key)[0:4] == "api_":
+            if dict_line[key] > maxi:
+                maxi = dict_line[key]
+    return maxi
+
+def count_min_same_api_call(dict_line):
+    mini = 1e20
+    for key in dict_line:
+        if str(key)[0:4] == "api_":
+            if dict_line[key] < mini:
+                mini = dict_line[key]
+    return mini
+
+def count_moy_api_call(dict_line):
+    summ = 0.0
+    counter = 0
+    for key in dict_line:
+        if str(key)[0:4] == "api_":
+            summ += dict_line[key]
+            counter += 1
+    if counter != 0:
+        return summ/counter
+    else:
+        return 0.0
+
+def count_max_same_rsi_call(dict_line):
+    maxi = 0
+    for key in dict_line:
+        if str(key)[0:4] != "api_" and "." not in str(key):
+            if dict_line[key] > maxi:
+                maxi = dict_line[key]
+    return maxi
+
+def count_min_same_rsi_call(dict_line):
+    mini = 1e20
+    for key in dict_line:
+        if str(key)[0:4] != "api_" and "." not in str(key):
+            if dict_line[key] < mini:
+                mini = dict_line[key]
+    return mini
+
+def count_moy_rsi_call(dict_line):
+    summ = 0.0
+    counter = 0
+    for key in dict_line:
+        if str(key)[0:4] != "api_" and "." not in str(key):
+            summ += dict_line[key]
+            counter += 1
+    if counter != 0:
+        return summ/counter
+    else:
+        return 0.0
+
+def count_max_api_call_par_process(dict_line):
+    maxi = 0
+    for key in dict_line:
+        if "." in str(key):
+            if dict_line[key] > maxi:
+                maxi = dict_line[key]
+    return maxi
+
+def count_min_api_call_par_process(dict_line):
+    mini = 1e20
+    for key in dict_line:
+        if "." in str(key):
+            if dict_line[key] < mini:
+                mini = dict_line[key]
+    return mini
+
+def count_moy_api_call_par_process(dict_line):
+    summ = 0.0
+    counter = 0
+    for key in dict_line:
+        if "." in str(key):
+            summ += dict_line[key]
+            counter += 1
+    if counter != 0:
+        return summ/counter
+    else:
+        return 0.0
+
+def count_nb_api(dict_line):
+    counter = 0
+    for key in dict_line:
+        if str(key)[0:4] == "api_":
+            counter += 1
+    return counter
+
+def count_nb_rsi(dict_line):
+    counter = 0
+    for key in dict_line:
+        if str(key)[0:4] != "api_" and "." not in str(key):
+            counter += 1
+    return counter
+
+#Total Nb process generation
+np.apply_along_axis(lambda line: X.append(len(line[0])), 1, matrix_test)
+#Total Nb api/rsi calls
+np.apply_along_axis(lambda line: X.append(len(line[1])), 1, matrix_test)
 
 
+#Nb min d'api call par process
+for i in range(0, len(npcount_line_tab)):
+    X.append(count_min_api_call_par_process(npcount_line_tab[i]))
+#Nb max d'api call par process
+for i in range(0, len(npcount_line_tab)):
+    X.append(count_max_api_call_par_process(npcount_line_tab[i]))
+#Nb moy d'api call par process
+for i in range(0, len(npcount_line_tab)):
+    X.append(count_moy_api_call_par_process(npcount_line_tab[i]))
 
-### III- feature engineering
+#Nb min same api call
+for i in range(0, len(npcount_line_tab)):
+    X.append(count_min_same_api_call(npcount_line_tab[i]))
+#Nb max same api call
+for i in range(0, len(npcount_line_tab)):
+    X.append(count_max_same_api_call(npcount_line_tab[i]))
+#Nb moy same api call
+for i in range(0, len(npcount_line_tab)):
+    X.append(count_moy_api_call(npcount_line_tab[i]))
 
+#Nb min same rsi call
+for i in range(0, len(npcount_line_tab)):
+    X.append(count_min_same_rsi_call(npcount_line_tab[i]))
+#Nb max same rsi call
+for i in range(0, len(npcount_line_tab)):
+    X.append(count_max_same_rsi_call(npcount_line_tab[i]))
+#Nb moy same rsi call
+for i in range(0, len(npcount_line_tab)):
+    X.append(count_moy_rsi_call(npcount_line_tab[i]))
 
-###IV- Algorithms ### (use gridsearch)
+#Nb api differentes
+for i in range(0, len(npcount_line_tab)):
+    X.append(count_nb_api(npcount_line_tab[i]))
+#Nb rsi differentes
+for i in range(0, len(npcount_line_tab)):
+    X.append(count_nb_rsi(npcount_line_tab[i]))
 
-#1. Naïve Bayes
-gnb = GaussianNB()
-gnb.fit(X_train, y_train)
-print("Accuracy score (training): {0:.4f}".format(gnb.score(X_train, y_train)))
-print("Accuracy score (testing): {0:.4f}".format(gnb.score(X_test, y_test)))
+npX = np.reshape(X, (p, N))
 
-y_scores_gnb = gnb.decision_function(X_test)
-fpr_gnb, tpr_gnb, threshold = roc_curve(list(y_test), y_scores_gnb)
-roc_auc_gnb = auc(fpr_gnb, tpr_gnb)
+end_time = time.time()
+print("----------------------------------------Done in "+str(end_time-start_time)+" s.------------------------------------------------------")
+start_time = time.time()
 
-print("Area under ROC curve = {:0.2f}".format(roc_auc_gnb))
-#plt.plot(fpr_gnb, tpr_gnb)
-#plt.xlabel('False positive rate')
-#plt.ylabel('True positive rate')
-#plt.title('ROC curve')
-#plt.legend(loc='Gaussian Naive Bayes')
-#plt.show()    
-#2. SVM 
-C= list(range(100)) #Tune on this param or min_samples_split or max_features
-param_grid={'C':C}
-svm = SVC(kernel='rbf)
-best_svm=GridSearchCV(svm, param_grid, cv=20)
-best_svm.fit(X_train, y_train)
-print("Learning rate: ", best_svm.best_params_)
-print("Accuracy score (training): {0:.4f}".format(best_svm.score(X_train, y_train)))
-print("Accuracy score (testing): {0:.4f}".format(best_svm.score(X_test, y_test)))
+print("----------------------------------------Save in file------------------------------------------------------")
+np.savetxt("X.csv", npX, delimiter=",")
 
-y_scores_svm = svm.decision_function(X_test)
-fpr_svm, tpr_svm, threshold = roc_curve(list(y_test), y_scores_svm)
-roc_auc_svm = auc(fpr_svm, tpr_svm)
+end_time = time.time()
+print("----------------------------------------Done in "+str(end_time-start_time)+" s.------------------------------------------------------")
+start_time = time.time()
 
-print("Area under ROC curve = {:0.2f}".format(roc_auc_svm))
-#plt.plot(fpr_svm, tpr_svm)
-#plt.xlabel('False positive rate')
-#plt.ylabel('True positive rate')
-#plt.title('ROC curve')
-#plt.legend(loc='best for SVM')
-#plt.show()    
+print("----------------------------------------Printing------------------------------------------------------")
+#pprint.pprint(matrix_test)
+#pprint.pprint(npcount_line_tab)
+#pprint.pprint(npX)
 
-#3. Decision Trees
-max_depth= list(range(20)) #Tune on this param or min_samples_split or max_features
-param_grid={'max_depth':max_depth}
-dt = DecisionTreeClassifier()
-best_dt=GridSearchCV(dt, param_grid, cv=20)
-best_dt.fit(X_train, y_train)
-#print("Learning rate: ", best_dt.best_params_)
-#print("Accuracy score (training): {0:.4f}".format(best_dt.score(X_train, y_train)))
-#print("Accuracy score (testing): {0:.4f}".format(best_dt.score(X_test, y_test)))
+end_time = time.time()
+print("----------------------------------------Done in "+str(end_time-start_time)+" s.------------------------------------------------------")
+start_time = time.time()
 
-y_scores_dt = dt.decision_function(X_test)
-fpr_dt, tpr_dt, threshold = roc_curve(list(y_test), y_scores_dt)
-roc_auc_dt = auc(fpr_dt, tpr_dt)
+print("----------------------------------------Plotting------------------------------------------------------")
+for i in range(0, p):
+    plt.clf()
+    df = pd.DataFrame(data=npX[:, i], index=np.arange(0,p), columns=[0])
+    svm = sns.distplot(df)
+    figure = svm.get_figure() 
+    figure.savefig("figures/output_feature_"+str(i)+".png")
 
-#print("Area under ROC curve = {:0.2f}".format(roc_auc_dt))
-#plt.plot(fpr_dt, tpr_dt)
-#plt.xlabel('False positive rate')
-#plt.ylabel('True positive rate')
-#plt.title('ROC curve')
-#plt.legend(loc='best for Decision Trees')
-#plt.show()    
+end_time = time.time()
+print("----------------------------------------Done in "+str(end_time-start_time)+" s.------------------------------------------------------")
+start_time = time.time()
 
+#Min process generation
+#Max process generation
+#Threshold process generation -> np.where(y_tr >= 1800, 1, 0)
 
+#sequence of behaviors
+#nb/min/max of subprocess id
+#api is greater than 1
+#api is 0
 
-#4. RandomForest
-estimators = [10,15] #Tune on this param or max_depth, or max_features
-param_grid={'n_estimators':estimators}
-rf = RandomForestClassifier()
-best_rf=GridSearchCV(rf, param_grid, cv=20)
-best_rf.fit(X_train, y_train)
-print("Learning rate: ", best_rf.best_params_)
-print("Accuracy score (training): {0:.4f}".format(best_rf.score(X_train, y_train)))
-print("Accuracy score (testing): {0:.4f}".format(best_rf.score(X_test, y_test)))
-
-y_scores_rf = rf.decision_function(X_test)
-fpr_rf, tpr_rf, threshold = roc_curve(list(y_test), y_scores_rf)
-roc_auc_rf = auc(fpr_rf, tpr_rf)
-
-print("Area under ROC curve = {:0.2f}".format(roc_auc_rf))
-#plt.plot(fpr_rf, tpr_rf)
-#plt.xlabel('False positive rate')
-#plt.ylabel('True positive rate')
-#plt.title('ROC curve')
-#plt.legend(loc='best for RandomForest')
-#plt.show()    
-
-
-
-
-#5. Boosting
-learning_rates = [0.05, 0.1, 0.25, 0.5, 0.75, 1] #Tune on this param or n_estimators or max_features
-param_grid={'learning_rate':learning_rates}
-gb = GradientBoostingClassifier(n_estimators=20, max_features=2, max_depth = 2, random_state = 0)
-best_gb=GridSearchCV(gb, param_grid, cv=20)
-best_gb.fit(X_train, y_train)
-print("Learning rate: ", best_gb.best_params_)
-print("Accuracy score (training): {0:.4f}".format(best_gb.score(X_train, y_train)))
-print("Accuracy score (testing): {0:.4f}".format(best_gb.score(X_test, y_test)))
-
-y_scores_gb = gb.decision_function(X_test)
-fpr_gb, tpr_gb, threshold = roc_curve(list(y_test), y_scores_gb)
-roc_auc_gb = auc(fpr_gb, tpr_gb)
-
-print("Area under ROC curve = {:0.2f}".format(roc_auc_gb))
-#plt.plot(fpr_gb, tpr_gb)
-#plt.xlabel('False positive rate')
-#plt.ylabel('True positive rate')
-#plt.title('ROC curve')
-#plt.legend(loc='best for XGBoost')
-#plt.show()    
-
-          
-          
-#### Best classifier
-          
-Best_classifier=best_gb
-best_classifier.fit(X_train,y_train)
-y_pred=pd.DataFrame(best_classifier.predict(X_val))
-file_path=''
-file_name='answer.txt'
-zip_file_directory=''
-np.savetxt(file_path+file_name, y_pred.values, fmt='%d')
-#shutil.make_archive(file_path+file_name, 'zip',zip_file_directory)
-          
+#behavior graph has boucle
