3b172115bdf087c69db79138cf0402d4ab24d59f
antoinedelplace <34864698+antoinedelplace@users.noreply.github.com>
2018-11-10 04:07:46 +0100

    Update antoine.py

--- antoine.py
+++ antoine.py
@@ -13,33 +13,55 @@ warnings.filterwarnings("ignore")
 
 print("----------------------------------------Import matrix--------------------------------------------------------------")
 #matrix_test = np.array([[[('stub.exe_9f01000', 'sample.exe_1356d000')], ['sample.exe_1356d000,1fa617,api_1487\n', 'sample.exe_1356d000,1fa626,api_0496\n', 'sample.exe_1356d000,1fa62f,api_0338\n']], [[('stub.exe_9f01000', 'sample.exe_1356d000')], ['sample.exe_1356d000,1fa617,api_1487\n', 'sample.exe_1356d000,1fa626,api_0496\n', 'sample.exe_1356d000,1fa62f,api_0338\n']], [[('stub.exe_1319c000', 'sample.exe_11ad0000'), ('stub.exe_1319c000', 'sample.exe_11ad0000')], ['sample.exe_1356d000,1fa617,api_1487\n', 'sample.exe_1356d000,1fa626,api_0496\n', 'sample.exe_1356d000,1fa62f,api_0338\n']]])
-matrix_test = np.load("../palmyre/matrix.raw")
+matrix_test_total = np.load("../palmyre/matrix.raw")
+matrix_test = matrix_test_total[0:50, :]
 N = len(matrix_test)
 
-matrix_test_clean = []
-
 def clean(line):
-    matrix_test_clean.append([line[0], []])
     for i in range(0, len(line[1])):
         string = re.sub("\n", "", line[1][i])
-        matrix_test_clean[-1][1].append(string.split(","))
+        line[1][i] = np.array(string.split(","))
         
 print("----------------------------------------Cleaning--------------------------------------------------------------")
 np.apply_along_axis(lambda line: clean(line), 1, matrix_test)
 
 print("----------------------------------------Printing------------------------------------------------------")
-pprint.pprint(matrix_test_clean)
+pprint.pprint(matrix_test)
 
-p = 2 #Nb de features
+p = 3 #Nb de features
 
 X = []
 
 print("----------------------------------------Feature Engineering------------------------------------------------------")
+def count(line):
+    unique, counts = np.unique(line, return_counts=True)
+    return dict(zip(unique, counts))
+
+def count_api_by_process(line):
+    dict_count = count(line)
+    summ = 0.0
+    counter = 0
+    for key in dict_count:
+        try:
+            int(key, 16)
+        except: #not RSI
+            if str(key)[0:4] != "api_":
+                summ += dict_count[key]
+                counter += 1
+    if counter != 0:
+        return summ/counter
+    else:
+        return 0.0
+                
+                
+
 #Total Nb process generation
-np.apply_along_axis(lambda line: X.append(len(line[0])), 1, matrix_test_clean)
+np.apply_along_axis(lambda line: X.append(len(line[0])), 1, matrix_test)
 #Total Nb api calls
-np.apply_along_axis(lambda line: X.append(len(line[1])), 1, matrix_test_clean)
+np.apply_along_axis(lambda line: X.append(len(line[1])), 1, matrix_test)
 #Nb moyen d'api calls par process
+np.apply_along_axis(lambda line: X.append(count_api_by_process(line[1])), 1, matrix_test)
+
 
 npX = np.reshape(X, (p, N))
 
