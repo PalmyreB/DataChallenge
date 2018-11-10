9d69de944c55959f862ac64e533926061ffeda53
fatmamoalla <37354761+fatmamoalla@users.noreply.github.com>
2018-11-10 01:03:52 +0100

    Update Code Algos ML.py

--- Code Algos ML.py
+++ Code Algos ML.py
@@ -9,6 +9,7 @@ from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.metrics import roc_curve
 from sklearn.metrics import auc
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
 
 
 ###II- Loading data:
@@ -26,25 +27,25 @@ y_test=........
 
 
 #2. Decision Trees
-estimators = [10,15] #Tune on this param or max_depth, or max_features
-param_grid={'n_estimators':estimators}
-rf = RandomForestClassifier()
-best_rf=GridSearchCV(rf, param_grid, cv=20)
-best_rf.fit(X_train, y_train)
-print("Learning rate: ", best_rf.best_params_)
-print("Accuracy score (training): {0:.4f}".format(best_rf.score(X_train, y_train)))
-print("Accuracy score (testing): {0:.4f}".format(best_rf.score(X_test, y_test)))
+max_depth= list(range(20)) #Tune on this param or min_samples_split or max_features
+param_grid={'max_depth':max_depth}
+dt = DecisionTreeClassifier()
+best_dt=GridSearchCV(dt, param_grid, cv=20)
+best_dt.fit(X_train, y_train)
+print("Learning rate: ", best_dt.best_params_)
+print("Accuracy score (training): {0:.4f}".format(best_dt.score(X_train, y_train)))
+print("Accuracy score (testing): {0:.4f}".format(best_dt.score(X_test, y_test)))
 
-y_scores_rf = rf.decision_function(X_test)
-fpr_rf, tpr_rf, threshold = roc_curve(list(y_test), y_scores_rf)
-roc_auc_rf = auc(fpr_rf, tpr_rf)
+y_scores_dt = dt.decision_function(X_test)
+fpr_dt, tpr_dt, threshold = roc_curve(list(y_test), y_scores_dt)
+roc_auc_dt = auc(fpr_dt, tpr_dt)
 
-print("Area under ROC curve = {:0.2f}".format(roc_auc_rf))
-plt.plot(fpr_rf, tpr_rf)
+print("Area under ROC curve = {:0.2f}".format(roc_auc_dt))
+plt.plot(fpr_dt, tpr_dt)
 plt.xlabel('False positive rate')
 plt.ylabel('True positive rate')
 plt.title('ROC curve')
-plt.legend(loc='best for RandomForest')
+plt.legend(loc='best for Decision Trees')
 plt.show()    
 
 
