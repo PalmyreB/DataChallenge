1f596128ed35f5769066709f291ee2168c8399f3
antoinedelplace <34864698+antoinedelplace@users.noreply.github.com>
2018-11-10 03:01:34 +0100

    Update antoine.py

--- antoine.py
+++ antoine.py
@@ -1,16 +1,32 @@
+#! python3.6
+# -*- coding: cp1252 -*-
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import re
 from sklearn import linear_model, decomposition, metrics, model_selection, preprocessing
 from numpy import linalg
+import pprint
 
 import warnings
 warnings.filterwarnings("ignore")
 
 matrix_test = np.array([[[('stub.exe_9f01000', 'sample.exe_1356d000')], ['sample.exe_1356d000,1fa617,api_1487\n', 'sample.exe_1356d000,1fa626,api_0496\n', 'sample.exe_1356d000,1fa62f,api_0338\n']], [[('stub.exe_9f01000', 'sample.exe_1356d000')], ['sample.exe_1356d000,1fa617,api_1487\n', 'sample.exe_1356d000,1fa626,api_0496\n', 'sample.exe_1356d000,1fa62f,api_0338\n']], [[('stub.exe_1319c000', 'sample.exe_11ad0000'), ('stub.exe_1319c000', 'sample.exe_11ad0000')], ['sample.exe_1356d000,1fa617,api_1487\n', 'sample.exe_1356d000,1fa626,api_0496\n', 'sample.exe_1356d000,1fa62f,api_0338\n']]])
 
+matrix_test_clean = []
+
+def clean(line):
+    matrix_test_clean.append([line[0], []])
+    for i in range(0, len(line[1])):
+        string = re.sub("\n", "", line[1][i])
+        matrix_test_clean[-1][1].append(string.split(","))
+        
 ## Cleaning
-matrix_test.apply(lambda x: re.sub("\n", "", x))
+np.apply_along_axis(lambda x: clean(x), 1, matrix_test)
+
+print("------------------------------------------------------------------------------------------------------")
+
+pprint.pprint(matrix_test_clean)
 
 #Nb process generation
 #Min process generation
