48bb0e36365d09acc4a4c5d3f6d0bdc832d95264
antoinedelplace <34864698+antoinedelplace@users.noreply.github.com>
2018-11-10 03:14:54 +0100

    Update antoine.py

--- antoine.py
+++ antoine.py
@@ -11,7 +11,9 @@ import pprint
 import warnings
 warnings.filterwarnings("ignore")
 
-matrix_test = np.array([[[('stub.exe_9f01000', 'sample.exe_1356d000')], ['sample.exe_1356d000,1fa617,api_1487\n', 'sample.exe_1356d000,1fa626,api_0496\n', 'sample.exe_1356d000,1fa62f,api_0338\n']], [[('stub.exe_9f01000', 'sample.exe_1356d000')], ['sample.exe_1356d000,1fa617,api_1487\n', 'sample.exe_1356d000,1fa626,api_0496\n', 'sample.exe_1356d000,1fa62f,api_0338\n']], [[('stub.exe_1319c000', 'sample.exe_11ad0000'), ('stub.exe_1319c000', 'sample.exe_11ad0000')], ['sample.exe_1356d000,1fa617,api_1487\n', 'sample.exe_1356d000,1fa626,api_0496\n', 'sample.exe_1356d000,1fa62f,api_0338\n']]])
+#matrix_test = np.array([[[('stub.exe_9f01000', 'sample.exe_1356d000')], ['sample.exe_1356d000,1fa617,api_1487\n', 'sample.exe_1356d000,1fa626,api_0496\n', 'sample.exe_1356d000,1fa62f,api_0338\n']], [[('stub.exe_9f01000', 'sample.exe_1356d000')], ['sample.exe_1356d000,1fa617,api_1487\n', 'sample.exe_1356d000,1fa626,api_0496\n', 'sample.exe_1356d000,1fa62f,api_0338\n']], [[('stub.exe_1319c000', 'sample.exe_11ad0000'), ('stub.exe_1319c000', 'sample.exe_11ad0000')], ['sample.exe_1356d000,1fa617,api_1487\n', 'sample.exe_1356d000,1fa626,api_0496\n', 'sample.exe_1356d000,1fa62f,api_0338\n']]])
+matrix_test = np.load("../palmyre/matrix.raw")
+N = len(matrix_test)
 
 matrix_test_clean = []
 
@@ -22,13 +24,26 @@ def clean(line):
         matrix_test_clean[-1][1].append(string.split(","))
         
 ## Cleaning
-np.apply_along_axis(lambda x: clean(x), 1, matrix_test)
+np.apply_along_axis(lambda line: clean(line), 1, matrix_test)
 
 print("------------------------------------------------------------------------------------------------------")
 
 pprint.pprint(matrix_test_clean)
 
-#Nb process generation
+p = 2 #Nb de features
+
+X = []
+
+#Total Nb process generation
+np.apply_along_axis(lambda line: X.append(len(line[0])), 1, matrix_test_clean)
+#Total Nb api calls
+np.apply_along_axis(lambda line: X.append(len(line[1])), 1, matrix_test_clean)
+#Nb moyen d'api calls par process
+
+npX = np.reshape(X, (p, N))
+
+pprint.pprint(npX)
+
 #Min process generation
 #Max process generation
 #Threshold process generation -> np.where(y_tr >= 1800, 1, 0)
@@ -42,8 +57,6 @@ pprint.pprint(matrix_test_clean)
 #behavior graph has boucle
 #nombre d'api différentes
 #nombre d'adresses différentes
-
-#the total number of api_call
 
 
 # Set up a cross-validation with sklearn
