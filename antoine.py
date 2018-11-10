045064b5174fa984e570249db6f768f1b27876d4
antoinedelplace <34864698+antoinedelplace@users.noreply.github.com>
2018-11-10 05:22:00 +0100

    Update antoine.py

--- antoine.py
+++ antoine.py
@@ -26,39 +26,144 @@ print("----------------------------------------Cleaning-------------------------
 np.apply_along_axis(lambda line: clean(line), 1, matrix_test)
 
 print("----------------------------------------Printing------------------------------------------------------")
-#pprint.pprint(matrix_test)
+pprint.pprint(matrix_test)
 
-p = 3 #Nb de features
+p = 11 #Nb de features
 
 X = []
 
 print("----------------------------------------Feature Engineering------------------------------------------------------")
+count_line_tab = []
+
 def count(line):
     unique, counts = np.unique(line, return_counts=True)
     return dict(zip(unique, counts))
 
-def count_api_by_process(line):
-    dict_count = count(line)
+#Count
+np.apply_along_axis(lambda line: count_line_tab.append(count(line[1])), 1, matrix_test)
+
+npcount_line_tab = np.array(count_line_tab)
+print("----------------------------------------Printing------------------------------------------------------")
+#pprint.pprint(npcount_line_tab)
+
+def count_max_same_api_call(dict_line):
+    maxi = 0
+    for key in dict_line:
+        if str(key)[0:4] == "api_":
+            if dict_line[key] > maxi:
+                maxi = dict_line[key]
+    return maxi
+
+def count_min_same_api_call(dict_line):
+    mini = 1e20
+    for key in dict_line:
+        if str(key)[0:4] == "api_":
+            if dict_line[key] < mini:
+                mini = dict_line[key]
+    return mini
+
+def count_moy_api_call(dict_line):
     summ = 0.0
     counter = 0
-    for key in dict_count:
-        if str(key)[0:4] != "api_" and "." not in str(key):
-            summ += dict_count[key]
+    for key in dict_line:
+        if str(key)[0:4] == "api_":
+            summ += dict_line[key]
             counter += 1
     if counter != 0:
         return summ/counter
     else:
         return 0.0
-                
-                
+
+def count_max_same_rsi_call(dict_line):
+    maxi = 0
+    for key in dict_line:
+        if str(key)[0:4] != "api_" and "." not in str(key):
+            if dict_line[key] > maxi:
+                maxi = dict_line[key]
+    return maxi
+
+def count_min_same_rsi_call(dict_line):
+    mini = 1e20
+    for key in dict_line:
+        if str(key)[0:4] != "api_" and "." not in str(key):
+            if dict_line[key] < mini:
+                mini = dict_line[key]
+    return mini
+
+def count_moy_rsi_call(dict_line):
+    summ = 0.0
+    counter = 0
+    for key in dict_line:
+        if str(key)[0:4] != "api_" and "." not in str(key):
+            summ += dict_line[key]
+            counter += 1
+    if counter != 0:
+        return summ/counter
+    else:
+        return 0.0
+
+def count_max_api_call_par_process(dict_line):
+    maxi = 0
+    for key in dict_line:
+        if "." in str(key):
+            if dict_line[key] > maxi:
+                maxi = dict_line[key]
+    return maxi
+
+def count_min_api_call_par_process(dict_line):
+    mini = 1e20
+    for key in dict_line:
+        if "." in str(key):
+            if dict_line[key] < mini:
+                mini = dict_line[key]
+    return mini
+
+def count_moy_api_call_par_process(dict_line):
+    summ = 0.0
+    counter = 0
+    for key in dict_line:
+        if "." in str(key):
+            summ += dict_line[key]
+            counter += 1
+    if counter != 0:
+        return summ/counter
+    else:
+        return 0.0
 
 #Total Nb process generation
 np.apply_along_axis(lambda line: X.append(len(line[0])), 1, matrix_test)
-#Total Nb api calls
+#Total Nb api/rsi calls
 np.apply_along_axis(lambda line: X.append(len(line[1])), 1, matrix_test)
-#Nb moyen d'api calls par process
-np.apply_along_axis(lambda line: X.append(count_api_by_process(line[1])), 1, matrix_test)
 
+#Nb min d'api call par process
+for i in range(0, len(npcount_line_tab)):
+    X.append(count_min_api_call_par_process(npcount_line_tab[i]))
+#Nb max d'api call par process
+for i in range(0, len(npcount_line_tab)):
+    X.append(count_max_api_call_par_process(npcount_line_tab[i]))
+#Nb moy d'api call par process
+for i in range(0, len(npcount_line_tab)):
+    X.append(count_moy_api_call_par_process(npcount_line_tab[i]))
+
+#Nb min same api call
+for i in range(0, len(npcount_line_tab)):
+    X.append(count_min_same_api_call(npcount_line_tab[i]))
+#Nb max same api call
+for i in range(0, len(npcount_line_tab)):
+    X.append(count_max_same_api_call(npcount_line_tab[i]))
+#Nb moy same api call
+for i in range(0, len(npcount_line_tab)):
+    X.append(count_moy_api_call(npcount_line_tab[i]))
+
+#Nb min same rsi call
+for i in range(0, len(npcount_line_tab)):
+    X.append(count_min_same_rsi_call(npcount_line_tab[i]))
+#Nb max same rsi call
+for i in range(0, len(npcount_line_tab)):
+    X.append(count_max_same_rsi_call(npcount_line_tab[i]))
+#Nb moy same rsi call
+for i in range(0, len(npcount_line_tab)):
+    X.append(count_moy_rsi_call(npcount_line_tab[i]))
 
 npX = np.reshape(X, (p, N))
 
@@ -68,7 +173,6 @@ pprint.pprint(npX)
 #Min process generation
 #Max process generation
 #Threshold process generation -> np.where(y_tr >= 1800, 1, 0)
-#the total number of process generation
 
 #sequence of behaviors
 #nb/min/max of subprocess id
